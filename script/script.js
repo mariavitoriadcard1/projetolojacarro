05d146e2da07019596c12ee8371a5b720036f3ca
mariavitoriadcard1 <mariavitoriadcard1@gmail.com>
2026-04-24 15:05:40 -0300

    salvarrrr

--- script/script.js
+++ script/script.js
@@ -2,24 +2,14 @@ function salvarCarros(event) {
     //impedir de recarregar a página
     event.preventDefault();
 
-    let titulo = document.getElementById('title');
-    let preco = document.getElementById('preco');
-    let marca = document.getElementById('marca');
-    let modelo = document.getElementById('modelo');
+    let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
 
     let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
 
     let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
-
-    let lista = document.getElementById('listarCarros');
-
-    let card = document.createElement('div');
-
-    card.style.border = "1px solid #ccc";
-    card.style.padding = "10px";
-    card.style.marginBottom = "10px";
-
-    lista.appendChild(card);
 
     let carro = {
         titulo,
@@ -32,4 +22,8 @@ function salvarCarros(event) {
     let carros = JSON.parse(localStorage.getItem("carros")) || [];
     carros.push(carro);
     localStorage.setItem("carros", JSON.stringify(carros));
+
+    adicionarNaTela(carro);
+
+    document.querySelector("form").reset();
 }
