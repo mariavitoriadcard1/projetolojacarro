e222e877b271d9a591b656419a6fdc062e800865
mariavitoriadcard1 <mariavitoriadcard1@gmail.com>
2026-04-29 15:38:08 -0300

    salvar

--- script/script.js
+++ script/script.js
@@ -43,3 +43,9 @@ function adicionarNaTela(carro) {
 
     lista.appendChild(card);
 }
+
+window.onload = function () {
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
+
+    carros.forEach(carro => {adicionarNaTela});
+}
