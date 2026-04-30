84023260ea134906e19495786a5673fc5220221c
mariavitoriadcard1 <mariavitoriadcard1@gmail.com>
2026-04-30 16:00:28 -0300

    save

--- script/script.js
+++ script/script.js
@@ -34,6 +34,7 @@ function adicionarNaTela(carro) {
     card.classList.add('card');
 
     card.innerHTML = `
+    <img src = 'https://picsum.photos/250/150?random = ${Math.random()}'>
     <h3>${carro.titulo}</h3>
     <p><strong>Preço:</strong> R$ ${carro.preco}</p>
     <p><strong>Marca:</strong> ${carro.marca}</p>
